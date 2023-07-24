7ddc4bd60427c1908111017fe4bf54b102589542
nzztuhin <64777785+nzztuhin@users.noreply.github.com>
2023-07-24 21:14:19 +0600

    get report

--- src/test/java/com/it/bd/pages/LoginPage.java
+++ src/test/java/com/it/bd/pages/LoginPage.java
@@ -1,22 +1,30 @@
 package com.it.bd.pages;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.it.bd.drivers.PageDriver;
 import com.it.bd.utilities.CommonMethods;
+import com.it.bd.utilities.GetScreenShot;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+
+import java.io.IOException;
 
 
 public class LoginPage extends CommonMethods {
-    public LoginPage(){
+    ExtentTest test;
+    public LoginPage(ExtentTest test){
         PageFactory.initElements(PageDriver.getCurrentDriver(),this);
+        this.test = test;
     }
     @FindBys({
             @FindBy(xpath = "//input[@placeholder='Username']"),
             @FindBy(xpath = "//input[@name='username']")
     })
-    WebElement username;
+    WebElement userName;
 
     @FindBys({
             @FindBy(xpath = "//input[@placeholder='Password']"),
@@ -28,31 +36,61 @@ public class LoginPage extends CommonMethods {
     })
     WebElement loginButton;
 
-    public void login() throws InterruptedException {
+    public void login() throws InterruptedException, IOException {
         timeout();
         try {
-            if (username.isDisplayed()) {
-                username.sendKeys("Admin");
+            if(userName.isDisplayed()) {
+                userName.sendKeys("Admin");
                 timeout();
             }
-        } catch (Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Username is not locateable.Please check the error message.</b></p>");
+            Throwable t = new InterruptedException("Exception");
+            test.fail(t);
+            @SuppressWarnings("unused")
+            String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "usernameLocator");
+            String dest = System.getProperty("user.dir") + "\\screenshots\\" + "usernameLocator.png";
+            test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+            Assert.assertTrue(userName.isDisplayed());
+            PageDriver.getCurrentDriver().quit();
         }
+
         try {
-            if (password.isDisplayed()) {
+            if(password.isDisplayed()) {
                 password.sendKeys("admin123");
                 timeout();
             }
-        } catch (Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Password is not locateable.Please check the error message.</b></p>");
+            Throwable t = new InterruptedException("Exception");
+            test.fail(t);
+            @SuppressWarnings("unused")
+            String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "passwordLocator");
+            String dest = System.getProperty("user.dir") + "\\screenshots\\" + "passwordLocator.png";
+            test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+            Assert.assertTrue(password.isDisplayed());
+            PageDriver.getCurrentDriver().quit();
         }
+
         try {
             if (loginButton.isDisplayed()) {
                 loginButton.click();
                 timeout(5000);
+                test.pass("<p style=\"color:#85BC63; font-size:13px\"><b>You have successfully logged in.</b></p>");
+                String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "loginPass");
+                String dest = System.getProperty("user.dir") + "\\screenshots\\" + "loginPass.png";
+                test.pass(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
             }
-        } catch (Exception e){
-            System.out.println(e);
+        } catch (Exception e) {
+            test.fail("<p style=\"color:#FF5353; font-size:13px\"><b>Login Button is not locateable.Please check the error message.</b></p>");
+            Throwable t = new InterruptedException("Exception");
+            test.fail(t);
+            @SuppressWarnings("unused")
+            String screenShotPath = GetScreenShot.capture(PageDriver.getCurrentDriver(), "loginButtonLocator");
+            String dest = System.getProperty("user.dir") + "\\screenshots\\" + "loginButtonLocator.png";
+            test.fail(MediaEntityBuilder.createScreenCaptureFromPath(dest).build());
+            Assert.assertTrue(loginButton.isDisplayed());
+            PageDriver.getCurrentDriver().quit();
         }
     }
 }
--- src/test/java/com/it/bd/tests/LoginTest.java
+++ src/test/java/com/it/bd/tests/LoginTest.java
@@ -1,20 +1,36 @@
 package com.it.bd.tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 import com.it.bd.drivers.BaseDriver;
 import com.it.bd.drivers.PageDriver;
 import com.it.bd.pages.LoginPage;
+import com.it.bd.utilities.ExtentFactory;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+
 public class LoginTest extends BaseDriver {
+    ExtentReports report;
+    ExtentTest parentTest;
+    ExtentTest childTest;
     @BeforeTest
     public void openUrl(){
         PageDriver.getCurrentDriver().manage().window().maximize();
         PageDriver.getCurrentDriver().get(url);
+        report = ExtentFactory.getInstance();
+        parentTest = report.createTest("<p style=\"color:#FF6000; font-size:20px\"><b>ORANGE HRM</b></p>").assignAuthor("QA TEAM").assignDevice("Windows");
     }
-    @Test (priority = 0)
-    public void loginTest() throws InterruptedException {
-        LoginPage loginPage = new LoginPage();
+    @Test
+    public void loginTest() throws InterruptedException, IOException {
+        childTest = parentTest.createNode("<p style=\"color:#3E96E7; font-size:20px\"><b>LOGIN TEST</b></p>");
+        LoginPage loginPage = new LoginPage(childTest);
         loginPage.login();
+    }
+    @AfterClass
+    public void afterClass() {
+        report.flush();
     }
 }
